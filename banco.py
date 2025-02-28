e7eaeb0a06601b3c39c264cb611f261d08fa5336
Felipe Nascimento de Souza <fn23886@gmail.com>
2025-02-28 17:15:08 -0300

    Remover visualizador de arquivos Excel e adicionar criação de banco de dados SQLite

---
+++ banco.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+# Criando o banco de dados/ Conectando ao banco de dados
+
+conexao = sqlite3.connect('meubanco.bd')
+cursor = conexao.cursor()
+
+# Criando uma tabela 
+
+cursor.execute("""
+               CREATE TABLE IF NOT EXISTS clientes (
+               id INTERGER PRIMARY KEY AUTOINCREMENT,
+               nome TEXT NOT NULL,
+               idade INTERGER
+               )
+               """)
+
+# Inserindo dados na tabela
+
+cursor.execute("INSERT INTO usuarios (nome, idade) VALUES ('Felipe', 25)")
+cursor.execute("INSERT INTO usuarios (nome, idade) VALUES ('Débora', 22)")
+cursor.execute("INSERT INTO usuarios (nome, idade) VALUES ('Brenda', 28)")
+
+# Salvando as alterações
+
+conexao.commit()
+conexao.close()
+
+print('Banco de dados criado com sucesso!')
